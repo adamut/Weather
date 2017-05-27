cc616d5ea315b976b60d1e712170efdf2b17e470
Dan <nohaidany96@gmail.com>
2017-05-27 12:47:56 +0300

    some design changes

--- app/src/main/java/com/weather/weather/ecranPornire.java
+++ app/src/main/java/com/weather/weather/ecranPornire.java
@@ -1,31 +1,28 @@
 package com.weather.weather;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class ecranPornire extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        //hide Blue ActionBar from ecranPornire
+        actionBar.hide();
         setContentView(R.layout.activity_ecran_pornire);
 
+        changeButtonStart();
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
+    public void changeButtonStart() {
+        Button button = (Button) findViewById(R.id.button);
+        button.getBackground().setAlpha(200);
     }
 
     public void toSearchCity(View view)
