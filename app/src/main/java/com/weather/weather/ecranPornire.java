d9c0ae0151da8cca82016caa7854a802fbc124d4
Dan <nohaidany96@gmail.com>
2017-05-27 12:38:12 +0300

    some design changes

--- app/src/main/java/com/weather/weather/ecranPornire.java
+++ app/src/main/java/com/weather/weather/ecranPornire.java
@@ -12,8 +12,22 @@ public class ecranPornire extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecran_pornire);
 
-
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
     public void toSearchCity(View view)
     {
         Intent intent = new Intent(ecranPornire.this, SearchCity.class);
