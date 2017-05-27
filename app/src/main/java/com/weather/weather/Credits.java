434db4be044948ae6c3cf37e2e889afa76061c84
Dan <nohaidany96@gmail.com>
2017-05-27 12:51:03 +0300

    some design changes

--- app/src/main/java/com/weather/weather/Credits.java
+++ app/src/main/java/com/weather/weather/Credits.java
@@ -1,31 +1,28 @@
 package com.weather.weather;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Credits extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        //hide Blue ActionBar from Credits
+        actionBar.hide();
         setContentView(R.layout.activity_credits);
+        changeButtonCredits();
     }
-    //Full screen
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
+    public void changeButtonCredits() {
+        Button button = (Button) findViewById(R.id.button);
+        button.getBackground().setAlpha(200);
     }
+
     public void toSearchCity(View view)
     {
         Intent intent = new Intent(Credits.this, SearchCity.class);
