9c2dfbf53564863e7a0fadcb81cf892f59ea5b9a
Dan <nohaidany96@gmail.com>
2017-05-27 13:15:30 +0300

    some design changes

--- app/src/main/java/com/weather/weather/ecranPornire.java
+++ app/src/main/java/com/weather/weather/ecranPornire.java
@@ -38,4 +38,24 @@ public class ecranPornire extends AppCompatActivity {
         startActivity(intent);
         finish();//distrugem activitatea
     }
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
 }
